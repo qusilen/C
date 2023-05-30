73d2cd8c8ff8906fff010f372d4662a861ff79b0
qusilen <87521424+qusilen@users.noreply.github.com>
2023-05-30 20:47:23 +0300

    buyuktenKucuge
    Girilen 3 adet sayıyı büyükten küçüğe sıralar.

---
+++ ifElse.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+int main() {
+
+	int a, b, c;
+	printf("Lutfen 3 sayi giriniz:");
+	scanf_s("%d %d %d", &a, &b, &c);
+
+	if (a > b && a>c) {
+		if (b > c) {
+			printf("%d>%d>%d", a, b, c);
+		}
+		else {
+			printf("%d>%d>%d", a, c, b);
+		}
+	}
+	else if (b > a && b > c) {
+		if (a> c) {
+			printf("%d>%d>%d", b, a, c);
+		}
+		else {
+			printf("%d>%d>%d", b, c, a);
+		}
+	}
+	else {
+		if (a > b) {
+			printf("%d>%d>%d", c, a, b);
+		}
+		else {
+			printf("%d>%d>%d", c, b, a);
+		}
+	}
+
+	return 0;
+}
